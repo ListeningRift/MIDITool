074e8841efb599eb7321b06b44ca2a80f3ba09ed
朱竞铭 <zhu.jingming@trs.com.cn>
2024-06-25 16:46:13 +0800

    fix: update logo

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import store from './store'
 import App from './App.vue'
 
-import './assets/fonts/iconfont.css'
 import '@/styles/index.less'
 
 export const app = createApp(App).use(store)
